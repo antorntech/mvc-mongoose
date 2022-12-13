14ca57470f88340716ce1d26279d57e423fb26c8
antor10655gh <creativefreelancerantor@gmail.com>
2022-12-13 13:10:02 +0600

    add initial file and package

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+
+const app = require("./app");
+
+// database connection
+mongoose.connect(process.env.DATABASE_LOCAL).then(()=>{
+    console.log('Database already connected!'.green.bold);
+})
+
+// server
+const port = process.env.PORT || 8080;
+
+app.listen(port, ()=>{
+    console.log(`App is running on ${port}`.yellow.bold);
+})
